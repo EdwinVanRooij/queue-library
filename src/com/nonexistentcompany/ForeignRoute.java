15c7a4ad21f77cf08310a55825f895cfc7b04804
Edwin van Rooij <edwinvrooij@gmail.com>
2018-05-18 10:48:53 +0200

    completed untested flow chart

--- src/com/nonexistentcompany/ForeignRoute.java
+++ src/com/nonexistentcompany/ForeignRoute.java
@@ -1,10 +1,16 @@
 package com.nonexistentcompany;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ForeignRoute {
     private List<Location> locationList;
     private String countryCode;
+
+    public ForeignRoute(String countryCode) {
+        this.countryCode = countryCode;
+        this.locationList = new ArrayList<>();
+    }
 
     public ForeignRoute(List<Location> locationList, String countryCode) {
         this.locationList = locationList;
@@ -17,6 +23,10 @@ public class ForeignRoute {
 
     public void setCountryCode(String countryCode) {
         this.countryCode = countryCode;
+    }
+
+    public void addLocation(Location location) {
+        this.locationList.add(location);
     }
 
     public List<Location> getLocationList() {
